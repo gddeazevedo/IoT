6d3329af0270fb8569fa8015b0e323ffb56bfb3a
CaioNP20 <79158437+CaioNP20@users.noreply.github.com>
2025-12-02 11:21:03 -0300

    Incluindo outros parâmetros

--- codigo_planilha.js
+++ codigo_planilha.js
@@ -5,13 +5,16 @@ function doGet(e) {
   try {
     var sheet = SpreadsheetApp.getActiveSpreadsheet().getSheetByName(SHEET_NAME);
 
-    // Pega os parâmetros da URL
-    var temperature = e.parameter.temperature;
-    var humidity = e.parameter.humidity;
-    var timestamp = new Date(); // Gera o obj data no momento que ele recebe a mensagem do esp
+    // Pega os parâmetros da URL (agora incluindo id e latitude)
+    var id = e.parameter.id; // ToDo: alterar para valor real
+    var latitude = e.parameter.lat; // ToDo: alterar para valor real
+    var longitude = e.parameter.lon;
+    var temperatura = e.parameter.temperature;
+    var umidade = e.parameter.humidity;
+    var timestamp = new Date; // Gera o obj data no momento que ele recebe a mensagem do esp
 
     // Adiciona uma nova linha com os dados
-    sheet.appendRow([timestamp, temperature, humidity]);
+    sheet.appendRow([id, temperatura, umidade, latitude, longitude, timestamp]);
 
     // Retorna uma resposta de sucesso
     return ContentService.createTextOutput("Dados recebidos com sucesso.").setMimeType(ContentService.MimeType.TEXT);
